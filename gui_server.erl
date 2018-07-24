795b13c31f7cf9d94de13f27951e63ddde30a74b
bary345 <bary345@gmail.com>
2018-07-24 19:00:47 +0300

    asdasd

--- gui_server.erl
+++ gui_server.erl
@@ -10,11 +10,27 @@
 %-record(pic_state, {picture_name, owner_name, pos, mov}).
 %-define(TIME, 100).
 
--define(WIDTH, 960).
--define(HEIGHT, 720).
+-define(WIDTH, 1920).
+-define(HEIGHT, 950).
+-define(BACKGROUND,      "/home/chenb/IdeaProjects/untitled/background.jpg").
 -define(RECEIVE_FOLDER,  "/home/chenb/IdeaProjects/untitled/receive_folder").
 -define(RESOURCE_FOLDER, "/home/chenb/IdeaProjects/untitled/resources_folder").
 
+
+update_ets('$end_of_table') -> ok;
+update_ets(Line) ->
+  {PictureName, Owner, Pos, Mov} = Line,
+  {NewPos, NewMov} = update_pos(Pos, Mov),
+  ets:delete(data_base, PictureName),
+  ets:insert(data_base, {{PictureName, Owner,NewPos, NewMov}}),
+  update_ets(ets:next(data_base,Line)).
+
+loop(Frame) ->
+
+  show_graphics(Frame),
+  receive after 10 -> ok end,
+  update_ets(ets:first(data_base)),
+  loop(Frame).
 
 start() ->
   wx:new(),
@@ -24,9 +40,9 @@ start() ->
   ets:new(data_base, [named_table, public, set]), % Any process can read or write to the table and the table is registered under data_base name.
 
   spawn( fun() -> file_scanner(?RECEIVE_FOLDER, ?RESOURCE_FOLDER) end),
-  receive after 1000 -> ok end,
+  receive after 200 -> ok end,
 
-  show_graphics(Frame).
+  loop(Frame).
 
 % ------------------------------------------------- %
 
@@ -51,9 +67,9 @@ iterate_update_move([H1|Filenames],[H2|Filenames_Dir], ResFolder)->
 random_movement() ->
   R = rand:uniform(),
   if R < 0.5 ->
-    -4;
+    -10;
   true
-    -> 4
+    -> 10
 end .
 
 get_node_to_transmit([HNodes|[]], _, [_|[]]) -> HNodes;
@@ -72,25 +88,33 @@ insert_picture(PictureName) ->
   %Picture = #pic_state{picture_name = PictureName, owner_name = Owner, pos = {PosX, PosY}, mov = {MovX, MovY}},
   Picture = {PictureName, Owner, {PosX, PosY},{MovX, MovY}},
   % need to send data to dest node.
-  ets:insert(data_base, {Picture, PictureName}).
+
+  %ets:insert(data_base, {Picture, PictureName}).
+  %ets:insert(data_base, {PictureName, Picture}).
+  ets:insert(data_base, {Picture}).
 
 % ------------------------------------------------- %
 
 show_graphics(Frame) ->
+  ClientDC = wxClientDC:new(Frame),
+  %wxDC:clear(ClientDC),%??
+  BufferDC = wxBufferedDC:new(ClientDC),
+  wxDC:drawBitmap(BufferDC,  wxBitmap:new(?BACKGROUND), {0,0}),
+  %wxDC:drawBitmap(BufferDC,  wxBitmap:new(?BACKGROUND,?WIDTH, ?HEIGHT), {0,0}),
+  receive after 10 -> ok end,
+
   First = ets:first(data_base),
   case First of
     '$end_of_table' -> ok;
-    _ ->
-      ClientDC = wxClientDC:new(Frame),
-      BufferDC = wxBufferedDC:new(ClientDC),
-      show_graphics(First, BufferDC),
-      wxBufferedDC:destroy(BufferDC),
-      wxClientDC:destroy(ClientDC)
-  end.
+    _ -> show_graphics(First, BufferDC)
+  end,
+  wxBufferedDC:destroy(BufferDC),
+  wxClientDC:destroy(ClientDC).
 
-show_graphics(Line, BufferDC)->
+show_graphics(Line, BufferDC) ->
+  %io:format("~s~n",[Line]),
   {PictureName, _, Pos, _} = Line,
-  wxDC:drawBitmap(BufferDC,  wxBitmap:new(PictureName), {10,10}),
+  wxDC:drawBitmap(BufferDC,  wxBitmap:new(PictureName), Pos),
   receive after 10 -> ok end,
   NextLine = ets:next(data_base, Line),
   case NextLine of
@@ -98,25 +122,22 @@ show_graphics(Line, BufferDC)->
     _			          -> show_graphics(NextLine, BufferDC)
   end.
 
-update_pos(Curr_pos,Mov)->
+% --------------------------------------- %
+
+update_pos({Xpos,Ypos},{Xmov,Ymov})->
   %hit the upper part
-  Xpos = element(1,Curr_pos),
-  Ypos = element(2,Curr_pos),
-  Xmov = element(1,Mov),
-  Ymov = element(2,Mov),
-  if Ypos =:= ?HEIGHT->
+  if Ypos >= ?HEIGHT->
     DirY = -1*Ymov;
-  true->
-    if Ypos =:= 0 ->
-      DirY = -1*Ymov;
     true->
-      DirY = Ymov
-    end
+      if Ypos =< 0 ->
+        DirY = -1*Ymov;
+        true -> DirY = Ymov
+      end
   end,
-  if Xpos =:= ?WIDTH->
+  if Xpos >= ?WIDTH->
     DirX = -1*Xmov;
     true->
-      if Xpos =:= 0 ->
+      if Xpos =< 0 ->
         DirX = -1*Xmov;
         true->
           DirX = Xmov
