e9fbc26c0941b9938842fd43586e40aaa85c7878
bary345 <bary345@gmail.com>
2018-07-25 17:35:22 +0300

    good

--- gui_server.erl
+++ gui_server.erl
@@ -5,8 +5,10 @@
 %% API
 -compile(export_all).
 -include_lib("wx/include/wx.hrl").
--include_lib("defines.hrl").
+%-include_lib("defines.hrl").
 
+-define(NODES,['shch1@127.0.0.1','shch2@127.0.0.1','shch3@127.0.0.1','shch4@127.0.0.1']).
+-define(PICTURE_RADIUS, 50).
 -define(WIDTH, 1920).
 -define(HEIGHT, 950).
 -define(DRAW_TIMEOUT, 10).
@@ -14,22 +16,7 @@
 -define(RECEIVE_FOLDER,  "/home/chenb/IdeaProjects/untitled/receive_folder").
 -define(RESOURCE_FOLDER, "/home/chenb/IdeaProjects/untitled/resources_folder").
 
-timeout(T) -> receive after T -> ok end.
-
-update_ets('$end_of_table') -> ok;
-update_ets(Line) ->
-  {PictureName, Owner, Pos, Mov} = Line,
-  {NewPos, NewMov} = update_pos(Pos, Mov),
-  NextLine = ets:next(data_base,Line),
-  ets:delete(data_base, Line),
-  ets:insert(data_base, {{PictureName, Owner, NewPos, NewMov}}),
-  update_ets(NextLine).
-
-loop(Frame) ->
-  show_graphics(Frame),
-  timeout(20),
-  update_ets(ets:first(data_base)),
-  loop(Frame).
+% ------------------------------------------------- %
 
 start() ->
   wx:new(),
@@ -39,6 +26,21 @@ start() ->
   spawn( fun() -> file_scanner(?RECEIVE_FOLDER, ?RESOURCE_FOLDER) end),
   timeout(200),
   loop(Frame).
+
+loop(Frame) ->
+  show_graphics(Frame),
+  timeout(20),
+  update_ets(ets:first(data_base)),
+  loop(Frame).
+
+update_ets('$end_of_table') -> ok;
+update_ets(Picture_Name) ->
+  [{Name, Owner, Pos, Mov}] = ets:lookup(data_base, Picture_Name),
+  {NewPos, NewMov} = update_pos(Pos, Mov),
+  NextLine = ets:next(data_base,Picture_Name),
+  ets:delete(data_base, Picture_Name),
+  ets:insert(data_base, {Name, Owner, NewPos, NewMov}),
+  update_ets(NextLine).
 
 % ------------------------------------------------- %
 
@@ -58,32 +60,37 @@ iterate_update_move([H1|Filenames],[H2|Filenames_Dir], ResFolder)->
 
 % ------------------------------------------------- %
 
+get_distace({Pos_X, Pos_Y}, {Picture_Pos_X, Picture_Pos_Y}) ->
+  math:pow(math:pow(Pos_X - Picture_Pos_X, 2) + math:pow(Pos_Y - Picture_Pos_Y, 2), 0.5).
+
+timeout(T) -> receive after T -> ok end.
+
 random_movement() ->
   R = rand:uniform(),
-  if R < 0.5 ->
-    -10;
-  true
-    -> 10
-end .
+  if R < 0.5 -> -10;
+    true -> 10
+  end .
 
 get_node_to_transmit([HNodes|[]], _, [_|[]]) -> HNodes;
 get_node_to_transmit([HNodes|_], Min, [Min|_]) -> HNodes;
 get_node_to_transmit([_|TNodes], Min, [_|T]) -> get_node_to_transmit(TNodes, Min, T).
 
-set_owner(Nodes) ->
-  NodesNumOfPictures = [ length(X) || X <- [ ets:lookup(data_base, X) || X <- Nodes]],
-  Min = lists:min(NodesNumOfPictures),
-  get_node_to_transmit(Nodes, Min, NodesNumOfPictures).
+% TODO: make it work following our logic.
+set_owner(Nodes) -> ok.
+%set_owner(Nodes) -> this code old as fuck.
+%  NodesNumOfPictures = [ length(X) || X <- [ ets:lookup(data_base, X) || X <- Nodes]],
+%  Min = lists:min(NodesNumOfPictures),
+%  get_node_to_transmit(Nodes, Min, NodesNumOfPictures).
 
-insert_picture(PictureName) ->
+insert_picture(Picture_Name) ->
   {PosX, PosY} = {trunc(?WIDTH * rand:uniform()), trunc(?HEIGHT * rand:uniform())},
   {MovX, MovY} = {random_movement(), random_movement()},
   Owner = set_owner(?NODES),
-  Picture = {list_to_atom(PictureName), Owner, {PosX, PosY},{MovX, MovY}},
+  Picture = {list_to_atom(Picture_Name), Owner, {PosX, PosY},{MovX, MovY}},
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   % here we need to send data to destenation node.%
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  ets:insert(data_base, {Picture}).
+  ets:insert(data_base, Picture).
 
 % ------------------------------------------------- %
 
@@ -103,15 +110,15 @@ show_graphics(Frame) ->
   wxBufferedDC:destroy(BufferDC),
   wxClientDC:destroy(ClientDC).
 
-show_graphics(Line, BufferDC) ->
-  {PictureName, _, Pos, _} = Line,
+show_graphics(Picture_Name, BufferDC) ->
+  [{Name, _, Pos, _}] = ets:lookup(data_base, Picture_Name),
 
-  Image = wxBitmap:new(atom_to_list(PictureName)),
+  Image = wxBitmap:new(atom_to_list(Name)),
   wxDC:drawBitmap(BufferDC, Image, Pos),
   timeout(?DRAW_TIMEOUT),
   wxBitmap:destroy(Image),
 
-  NextLine = ets:next(data_base, Line),
+  NextLine = ets:next(data_base, Picture_Name),
   case NextLine of
     '$end_of_table' -> ok;
     _			          -> show_graphics(NextLine, BufferDC)
@@ -119,18 +126,18 @@ show_graphics(Line, BufferDC) ->
 
 % --------------------------------------- %
 
-update_pos({Xpos,Ypos},{Xmov,Ymov})->
-  %hit the upper part
-  if Ypos >= ?HEIGHT -> DirY = -1*Ymov;
+%% TODO: fix that function
+update_pos({Pos_X, Pos_Y},{Movment_X, Movment_Y})->
+  if Pos_Y >= ?HEIGHT -> Direction_Y = -1 * Movment_Y;
     true ->
-      if Ypos =< 0 -> DirY = -1*Ymov;
-         true      -> DirY = Ymov
+      if Pos_Y =< 0  -> Direction_Y = -1 * Movment_Y;
+        true         -> Direction_Y = Movment_Y
       end
   end,
-  if Xpos >= ?WIDTH -> DirX = -1*Xmov;
+  if Pos_X >= ?WIDTH  -> Direction_X = -1 * Movment_X;
     true ->
-      if Xpos =< 0 -> DirX = -1*Xmov;
-         true      -> DirX = Xmov
+      if Pos_X =< 0  -> Direction_X = -1 * Movment_X;
+        true        -> Direction_X = Movment_X
       end
   end,
-  {{Xpos+DirX,Ypos+DirY},{DirX,DirY}}.
+  {{Pos_X + Direction_X, Pos_Y + Direction_Y}, {Direction_X, Direction_Y}}.
