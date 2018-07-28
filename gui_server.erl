35b6a05026d837fd5b1ed89520bd2343a9b17807
bary345 <bary345@gmail.com>
2018-07-28 13:14:31 +0300

    great movement with boom image

--- gui_server.erl
+++ gui_server.erl
@@ -6,6 +6,8 @@
 -include_lib("wx/include/wx.hrl").
 -include_lib("defines.hrl").
 
+-define(BOOM, "BOOM.jpg").
+
 -define(NODES,['shch1@127.0.0.1','shch2@127.0.0.1','shch3@127.0.0.1','shch4@127.0.0.1']).
 -define(ImgEdge, 50).
 -define(RADIUS, 25).
@@ -17,6 +19,8 @@
 -define(GRAPHICS_LOOP_TIMEOUT, 10).
 -define(PIC_PROCESS_TIMEOUT, 10).
 
+timeout(T) -> receive after T -> ok end.
+
 % ------------------------------------------------- %
 
 start() ->
@@ -25,30 +29,117 @@ start() ->
   Frame = wxFrame:new(wx:null(), -1, "Pictures",[{size,{?WIDTH,?HEIGHT}}]),
   wxFrame:show(Frame),
   ets:new(data_base, [named_table, public, set]),
+  ets:new(temporary_data_base, [named_table, public, set]),
   spawn( fun() -> file_scanner(?RECEIVE_FOLDER, ?RESOURCE_FOLDER,0) end),
-  register(master, spawn( fun() -> listener() end)),
+  register(master, spawn( fun() -> master_loop() end)),
   register(graphics_procces, self()),
   graphics_loop(Frame).
 
+% ------------------------------------------------- %
+
+master_loop() ->
+  receive
+    {PID, Type, Owner, Pos} -> graphics_procces ! {insert_temporary, PID, Type, Owner, Pos};
+    {kill_temporary, PID} -> graphics_procces ! {kill_temporary, PID};
+    {kill, Picture_Name} -> Picture_Name ! kill;
+    {self_kill, Picture_Name} -> graphics_procces ! {self_kill, Picture_Name};
+    {Picture_Name, NextPos, Movement} -> ets:update_element(data_base,Picture_Name,[{3,NextPos},{4,Movement}])
+  end,
+  master_loop().
+
+% ------------------------------------------------- %
+
 graphics_loop(Frame) ->
   receive
-    {insert_picture, {Pic_Name_Atom, Owner, {PosX, PosY},{MovX, MovY}}} ->
-      ets:insert(data_base, {Pic_Name_Atom, Owner, {PosX, PosY},{MovX, MovY}});
+    {insert_picture, {Picture_Name, Owner, {PosX, PosY},{MovX, MovY}}} ->
+      ets:insert(data_base, {Picture_Name, Owner, {PosX, PosY},{MovX, MovY}});
+
     {self_kill, Picture_Name} ->
       ets:delete(data_base, Picture_Name),
-      master ! {kill, Picture_Name}
+      master ! {kill, Picture_Name};
+
+    {insert_temporary, PID, Type, Owner, Pos} -> ets:insert(temporary_data_base, {PID, Type, Owner, Pos});
+
+    {kill_temporary, PID} -> ets:delete(temporary_data_base, PID)
   after 0 -> ok
   end,
-  %timeout(?GRAPHICS_LOOP_TIMEOUT),
   show_graphics(Frame),
   graphics_loop(Frame).
 
+show_graphics(Frame) ->
+  collision_detect(ets:first(data_base)),
+  ClientDC = wxClientDC:new(Frame),
+  BufferDC = wxBufferedDC:new(ClientDC),
+  Background = wxBitmap:new(?BACKGROUND),
+  wxDC:drawBitmap(BufferDC, Background, {0,0}),
+  timeout(?DRAW_TIMEOUT),
+  wxBitmap:destroy(Background),
 
-picture_process_loop(Picture_Name, _, _, _, _, _, 0) ->
+  show_graphics(ets:first(data_base), BufferDC),
+  show_temporary_graphics(ets:first(temporary_data_base), BufferDC),
+
+  wxBufferedDC:destroy(BufferDC),
+  wxClientDC:destroy(ClientDC).
+
+show_temporary_graphics('$end_of_table', _) -> ok;
+show_temporary_graphics(Line, BufferDC) ->
+  [{_, Type, _, Pos}] = ets:lookup(temporary_data_base, Line),
+  Image = wxBitmap:new(Type),
+  wxDC:drawBitmap(BufferDC, Image, Pos),
+  timeout(?DRAW_TIMEOUT),
+  wxBitmap:destroy(Image),
+  show_temporary_graphics(ets:next(temporary_data_base, Line), BufferDC).
+
+show_graphics('$end_of_table', _) -> ok;
+show_graphics(Picture_Name, BufferDC) ->
+  [{Name, _, Pos, _}] = ets:lookup(data_base, Picture_Name),
+  Image = wxBitmap:new(atom_to_list(Name)),
+  wxDC:drawBitmap(BufferDC, Image, Pos),
+  timeout(?DRAW_TIMEOUT),
+  wxBitmap:destroy(Image),
+  show_graphics(ets:next(data_base, Picture_Name), BufferDC).
+
+collision_detect('$end_of_table') -> ok;
+collision_detect(Picture) ->
+  Next_Picture = ets:next(data_base, Picture),
+  collision_detect(ets:lookup(data_base, Picture), Next_Picture),
+  collision_detect(Next_Picture).
+
+collision_detect(_, '$end_of_table') -> ok;
+collision_detect(Picture_Line, Picture_To_Campre) ->
+  Picture_Line_Campre = ets:lookup(data_base, Picture_To_Campre),
+  {Flag, Mov, MovCmp} = collision_check(Picture_Line, Picture_Line_Campre),
+  case Flag of
+    false -> collision_detect(Picture_Line, ets:next(data_base, Picture_To_Campre));
+    true  ->
+      [{Picture_Name, _, _, _}] = Picture_Line,
+      [{Picture_Name_Cmp, _, _, _}]= Picture_Line_Campre,
+      Picture_Name ! {collision, Mov},
+      Picture_Name_Cmp ! {collision, MovCmp}
+  end.
+
+collision_check(Line1, Line2) ->
+  [{_, _, {X1,Y1}, {MovX1, MovY1}}] = Line1,
+  [{_, _, {X2,Y2}, {MovX2, MovY2}}] = Line2,
+  Flag = (abs(X1-X2) =< ?ImgEdge) and (abs(Y1-Y2) =< ?ImgEdge),
+  case Flag of
+    true  -> {true, {MovX2, MovY2}, {MovX1, MovY1}};
+    false -> {false, false, false}
+  end.
+
+% ------------------------------------------------- %
+
+boom_process_loop(TTL) ->
+  timeout(TTL),
+  io:format("bye bye 2~n"),
+  master ! {kill_temporary, self()}.
+
+picture_process_loop(Picture_Name, Owner, Pos, _, _, _, 0) ->
   master ! {self_kill, Picture_Name},
   receive
-    kill -> io:format("bye bye~n")
-    %{Event, Picture} -> Event(Picture, 1000/?PIC_PROCESS_TIMEOUT)
+    kill -> io:format("bye bye 1~n"),
+            master ! {self(), ?BOOM, Owner, Pos},
+            boom_process_loop(1000)
   end;
 picture_process_loop(Picture_Name, Owner, {PosX, PosY},{MovX, MovY}, Were_Collision, TimeOut, TTL) ->
   Start_Timer = os:system_time(millisecond),
@@ -65,14 +156,6 @@ picture_process_loop(Picture_Name, Owner, {PosX, PosY},{MovX, MovY}, Were_Collis
     master ! {Picture_Name, NextPos, Movement},
     picture_process_loop(Picture_Name, Owner, NextPos, Movement, false, ?PIC_PROCESS_TIMEOUT, TTL)
   end.
-
-listener() ->
-  receive
-    {kill, Picture_Name} -> Picture_Name ! kill;
-    {self_kill, Picture_Name} -> graphics_procces ! {self_kill, Picture_Name};
-    {Picture_Name, NextPos, Movement} -> ets:update_element(data_base,Picture_Name,[{3,NextPos},{4,Movement}])
-  end,
-  listener().
 
 update_pos({Pos_X, Pos_Y}, {Movment_X, Movment_Y})->
   %%upper bound
@@ -99,35 +182,6 @@ update_pos({Pos_X, Pos_Y}, {Movment_X, Movment_Y})->
   end,
   {{Pos_X + Direction_X, Pos_Y + Direction_Y}, {Direction_X, Direction_Y}}.
 
-collision_detect('$end_of_table') -> ok;
-collision_detect(Picture) ->
-  Next_Picture = ets:next(data_base, Picture),
-  collision_detect(ets:lookup(data_base, Picture), Next_Picture),
-  collision_detect(Next_Picture).
-
-collision_detect(_, '$end_of_table') -> ok;
-collision_detect(Picture_Line, Picture_To_Campre) ->
-  Picture_Line_Campre = ets:lookup(data_base, Picture_To_Campre),
-  {Flag, Mov, MovCmp}= collision_check(Picture_Line, Picture_Line_Campre),
-  case Flag of
-    false -> collision_detect(Picture_Line, ets:next(data_base, Picture_To_Campre));
-    true  ->
-      [{Picture_Name, _, _, _}] = Picture_Line,
-      [{Picture_Name_Cmp, _, _, _}]= Picture_Line_Campre,
-      Picture_Name ! {collision, Mov},
-      Picture_Name_Cmp ! {collision, MovCmp}
-  end.
-
-%% this function work only for circle pictures.
-collision_check(Line1, Line2) ->
-  [{_, _, {X1,Y1}, {MovX1, MovY1}}] = Line1,
-  [{_, _, {X2,Y2}, {MovX2, MovY2}}] = Line2,
-  Flag = (abs(X1-X2) =< ?ImgEdge) and (abs(Y1-Y2) =< ?ImgEdge),
-  case Flag of
-    true  -> {true, {MovX2, MovY2}, {MovX1, MovY1}};
-    false -> {false, false, false}
-  end.
-
 % ------------------------------------------------- %
 
 file_scanner(RecFolder, ResFolder,Debug)->
@@ -149,7 +203,7 @@ iterate_update_move([H1| File_Names], [H2| File_Names_Dir], ResFolder,Debug)->
     insert_picture(ResFolder ++ "/" ++ H1);
     true->
       ok
-    end,
+  end,
   file:delete(H2),
   iterate_update_move(File_Names, File_Names_Dir, ResFolder,Debug).
 
@@ -165,7 +219,6 @@ insert_picture(Picture_Name) ->
   % TODO: here we need to send data to destenation node.%
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   graphics_procces ! {insert_picture, Picture},
-  %ets:insert(data_base, Picture),
   Pid = spawn_link(fun() -> picture_process_loop(Pic_Name_Atom, Owner, {PosX, PosY},{MovX, MovY}, false, ?PIC_PROCESS_TIMEOUT, ?TTL) end),
   register(Pic_Name_Atom, Pid).
 
@@ -187,36 +240,3 @@ set_owner(Nodes) -> ok.
 %  get_node_to_transmit(Nodes, Min, NodesNumOfPictures).
 
 % ------------------------------------------------- %
-
-show_graphics(Frame) ->
-  collision_detect(ets:first(data_base)),
-  ClientDC = wxClientDC:new(Frame),
-  BufferDC = wxBufferedDC:new(ClientDC),
-  Background = wxBitmap:new(?BACKGROUND),
-  wxDC:drawBitmap(BufferDC, Background, {0,0}),
-  timeout(?DRAW_TIMEOUT),
-  wxBitmap:destroy(Background),
-
-  First = ets:first(data_base),
-  case First of
-    '$end_of_table' -> ok;
-    _ -> show_graphics(First, BufferDC)
-  end,
-  wxBufferedDC:destroy(BufferDC),
-  wxClientDC:destroy(ClientDC).
-
-show_graphics(Picture_Name, BufferDC) ->
-  [{Name, _, Pos, _}] = ets:lookup(data_base, Picture_Name),
-  Image = wxBitmap:new(atom_to_list(Name)),
-  wxDC:drawBitmap(BufferDC, Image, Pos),
-  timeout(?DRAW_TIMEOUT),
-  wxBitmap:destroy(Image),
-  NextLine = ets:next(data_base, Picture_Name),
-  case NextLine of
-    '$end_of_table' -> ok;
-    _			          -> show_graphics(NextLine, BufferDC)
-  end.
-
-% --------------------------------------- %
-
-timeout(T) -> receive after T -> ok end.
