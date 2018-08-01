80662f1e7657fb08b21e5809687128c43963ebaf
bary345 <bary345@gmail.com>
2018-08-02 00:40:50 +0300

    great!

--- gui_otp_server.erl
+++ gui_otp_server.erl
@@ -6,7 +6,7 @@
 -behaviour(gen_server).
 
 %% API
--export([start/2, start_link/2]).
+-export([start/2, start_link/2, random_movement/0]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
@@ -33,10 +33,10 @@ init(Nodes) ->
   ets:new(data_base, [named_table, public, set]),
   ets:new(wait_for_approve_data_base, [named_table, public, set]),
   ets:new(temporary_data_base, [named_table, public, set]),
-  spawn_monitor(fun() -> graphics_init() end),
+
+  global:register_name(graphics, spawn(fun() -> graphics_init() end)),
   spawn_monitor(fun() -> init_scanner(?RECEIVED, ?RESOURCES) end),
   {ok, Nodes}.
-
 
 handle_call(_Request, _From, State) ->
   io:format("gui_otp_server not support call functions.~n"),
@@ -54,16 +54,18 @@ code_change(_OldVsn, State, _Extra) ->
 %%%                     Messages from Nodes                        %%%
 %%%===================================================================
 
-handle_cast({generate_position, Picture_Name, PosX, PosY, MovX, MovY}, Nodes) ->
-  graphics ! {generate_position, Picture_Name, PosX, PosY, MovX, MovY},
-  {noreply, Nodes};
+%%handle_cast({update_position, Picture_Name, New_Pos, New_Mov}, Nodes) ->
+%%  graphics ! {update_position, Picture_Name, New_Pos, New_Mov},
+%%  {noreply, Nodes};
 
-handle_cast({update_position, Picture_Name, Pos, Mov}, Nodes) ->
-  graphics ! {update_position, {Picture_Name, Pos, Mov}},
+handle_cast({generate_position, Picture_Name, PosX, PosY}, Nodes) ->
+  global:send(graphics, {generate_position, Picture_Name, PosX, PosY}),
+  %graphics ! {generate_position, Picture_Name, PosX, PosY},
   {noreply, Nodes};
 
 handle_cast({self_kill, Picture_Name}, Nodes) ->
-  graphics ! {self_kill, Picture_Name},
+  global:send(graphics, {self_kill, Picture_Name}),
+  %graphics ! {self_kill, Picture_Name},
   {noreply, Nodes};
 
 handle_cast({self_kill, node, _Node}, Nodes) ->
@@ -84,22 +86,6 @@ handle_info({insert, Picture_Name, Delay, TTL}, Nodes) ->
   gen_server:cast({global, Node}, {insert, Picture_Name, Delay, TTL}),
   {noreply, New_Nodes_State};
 
-handle_info({generate_position, approved, Picture_Name, PosX, PosY, MovX, MovY}, Nodes) ->
-  [{_, Node}] = ets:lookup(wait_for_approve_data_base, Picture_Name),
-  ets:delete(wait_for_approve_data_base, Picture_Name),
-  graphics ! {insert_picture, {Picture_Name, Node, {PosX, PosY},{MovX, MovY}}},
-  {noreply, Nodes};
-
-handle_info({generate_position, reject, Picture}, Nodes) ->
-  [{_, Node}] = ets:lookup(wait_for_approve_data_base, Picture),
-  gen_server:cast({global, Node}, {generate_position, reject, Picture}),
-  {noreply, Nodes};
-
-handle_info({collision, Picture, NewMov}, Nodes) ->
-  [{_, Node, _, _, _}] = ets:lookup(data_base, Picture),
-  gen_server:cast({global, Node}, {collision, Picture, NewMov}),
-  {noreply, Nodes};
-
 handle_info(Info, State) ->
   io:format("gui_otp_server - handle_info: unknown message: ~p~n",[Info]),
   {noreply, State}.
@@ -108,59 +94,78 @@ handle_info(Info, State) ->
 %%% Internal functions
 %%%===================================================================
 
+random_movement() ->
+  R = rand:uniform(),
+  if R < 0.5 -> -2;
+    true -> 2
+  end .
+
+generate_position_check('$end_of_table', Picture_Name, PosX, PosY)  ->
+  % TODO - check if NODE is the right one
+  [{_, Node}] = ets:lookup(wait_for_approve_data_base, Picture_Name),
+  ets:delete(wait_for_approve_data_base, Picture_Name),
+  {MovX, MovY} = {random_movement(), random_movement()},
+  ets:insert(data_base, {Picture_Name, Node, {PosX, PosY}, {MovX, MovY}}),
+  gen_fsm:send_event({global, Picture_Name}, {generate_position, approved, {PosX, PosY}, {MovX, MovY}});
+
+generate_position_check(Line, Picture_Name, NewPosX, NewPosY) ->
+  [{_, _, {PosX, PosY}, _}] = ets:lookup(data_base, Line),
+  case ((abs(PosY - NewPosY) =< ?ImgEdge) and (abs(PosX - NewPosX) =< ?ImgEdge)) of
+    true  ->
+      gen_fsm:send_event({global, Picture_Name}, {generate_position, reject});
+    false ->
+      generate_position_check(ets:next(data_base, Line), Picture_Name, NewPosX, NewPosY)
+  end.
+
+% ------------------------------------------------- %
+
 graphics_init() ->
-  register(graphics, self()),
   wx:new(),
   Frame = wxFrame:new(wx:null(), -1, "Take-A-Pic",[{size,{?WIDTH,?HEIGHT}}]),
   wxFrame:show(Frame),
   graphics_process(Frame).
 
-graphics_process(Frame) ->
-  graphics_read_messages(20),
+graphics_update_messages(0) -> 0;
+graphics_update_messages(N) ->
   receive
-    {update_position, {Picture_Name, Pos, Mov}} ->
-      ets:update_element(data_base, Picture_Name, [{3, Pos}, {4, Mov}, {5, true}])
+    {update_position, Picture_Name, Pos, Mov} ->
+      ets:update_element(data_base, Picture_Name, [{3, Pos}, {4, Mov}])
   after 0 -> ok end,
-  show_graphics(Frame),
-  graphics_process(Frame).
+  graphics_update_messages(N-1).
 
-graphics_read_messages(0) -> ok;
-graphics_read_messages(N) ->
+graphics_message()  ->
   receive
-    {generate_position, Picture_Name, PosX, PosY, MovX, MovY} ->
-      generate_position_check(ets:first(data_base), Picture_Name, PosX, PosY, MovX, MovY);
+    {generate_position, Picture_Name, PosX, PosY} ->
+      generate_position_check(ets:first(data_base), Picture_Name, PosX, PosY),
+      graphics_message();
 
     {insert_temporary, PID, Type, Owner, Pos} ->
-      ets:insert(temporary_data_base, {PID, Type, Owner, Pos});
+      ets:insert(temporary_data_base, {PID, Type, Owner, Pos}),
+      graphics_message();
 
     {insert_picture, {Picture_Name, Owner, {PosX, PosY}, {MovX, MovY}}} ->
       io:format("Picture insert at position {~p, ~p}.~n", [PosX, PosY]),
-      ets:insert(data_base, {Picture_Name, Owner, {PosX, PosY}, {MovX, MovY}, true}),
-      gen_server:cast({global, Owner}, {generate_position, approved, Picture_Name, PosX, PosY});
+      ets:insert(data_base, {Picture_Name, Owner, {PosX, PosY}, {MovX, MovY}}),
+      gen_server:cast({global, Owner}, {generate_position, approved, Picture_Name, PosX, PosY}),
+      graphics_message();
 
     {self_kill, Picture_Name} ->
-      ets:delete(data_base, Picture_Name);
+      ets:delete(data_base, Picture_Name),
+      graphics_message();
 
     {kill_temporary, PID} ->
-      ets:delete(temporary_data_base, PID)
+      ets:delete(temporary_data_base, PID),
+      graphics_message()
+  after 0 -> ok end.
 
-  after 0 -> ok end,
-  graphics_read_messages(N - 1).
-
-generate_position_check('$end_of_table', Picture, NewPosX, NewPosY, MovX, MovY)  ->
-  gui_server ! {generate_position, approved, Picture, NewPosX, NewPosY, MovX, MovY};
-generate_position_check(Line, Picture, NewPosX, NewPosY, MovX, MovY) ->
-  [{_, _, {PosX, PosY},_, _}] = ets:lookup(data_base, Line),
-  case ((abs(PosY - NewPosY) =< ?ImgEdge) and (abs(PosX - NewPosY) =< ?ImgEdge)) of
-    true  ->
-      gui_server ! {generate_position, reject, Picture};
-    false ->
-      generate_position_check(ets:next(data_base, Line), Picture, NewPosX, NewPosY, MovX, MovY)
-  end.
+graphics_process(Frame) ->
+  graphics_message(),
+  graphics_update_messages(10),collision_detect
+  %collision_detect(ets:first(data_base))
+  show_graphics(Frame),
+  graphics_process(Frame).
 
 show_graphics(Frame) ->
-  collision_detect(ets:first(data_base)),
-
   ClientDC = wxClientDC:new(Frame),
   BufferDC = wxBufferedDC:new(ClientDC),
   Background = wxBitmap:new(?BACKGROUND),
@@ -171,6 +176,15 @@ show_graphics(Frame) ->
   wxBufferedDC:destroy(BufferDC),
   wxClientDC:destroy(ClientDC).
 
+show_graphics('$end_of_table', _) -> ok;
+show_graphics(Picture_Name, BufferDC) ->
+  [{Name, _, Pos, _}] = ets:lookup(data_base, Picture_Name),
+  Image = wxBitmap:new(atom_to_list(Name)),
+  wxDC:drawBitmap(BufferDC, Image, Pos),
+  receive after ?DRAW_TIMEOUT -> ok end,
+  wxBitmap:destroy(Image),
+  show_graphics(ets:next(data_base, Picture_Name), BufferDC).
+
 show_temporary_graphics('$end_of_table', _) -> ok;
 show_temporary_graphics(Line, BufferDC) ->
   [{_, Type, _, Pos}] = ets:lookup(temporary_data_base, Line),
@@ -180,24 +194,13 @@ show_temporary_graphics(Line, BufferDC) ->
   wxBitmap:destroy(Image),
   show_temporary_graphics(ets:next(temporary_data_base, Line), BufferDC).
 
-show_graphics('$end_of_table', _) -> ok;
-show_graphics(Picture_Name, BufferDC) ->
-  [{Name, _, Pos, _, _}] = ets:lookup(data_base, Picture_Name),
-  Image = wxBitmap:new(atom_to_list(Name)),
-  wxDC:drawBitmap(BufferDC, Image, Pos),
-  receive after ?DRAW_TIMEOUT -> ok end,
-  wxBitmap:destroy(Image),
-  show_graphics(ets:next(data_base, Picture_Name), BufferDC).
+% ------------------------------------------------- %
 
 collision_detect('$end_of_table') -> ok;
 collision_detect(Picture) ->
   Next_Picture = ets:next(data_base, Picture),
   Line = ets:lookup(data_base, Picture),
-  [{_, _, _, _, Collision_Flag}] = Line,
-  case Collision_Flag of
-      true  -> collision_detect(Line, Next_Picture);
-      false -> collision_detect(Next_Picture)
-  end.
+  collision_detect(Line, Next_Picture).
 
 collision_detect(_, '$end_of_table') -> ok;
 collision_detect(Picture_Line, Picture_To_Campre) ->
@@ -206,21 +209,19 @@ collision_detect(Picture_Line, Picture_To_Campre) ->
   case Flag of
     false -> collision_detect(Picture_Line, ets:next(data_base, Picture_To_Campre));
     true  ->
-      [{Picture_Name, _, _, _, _}] = Picture_Line,
-      [{Picture_Name_Cmp, _, _, _, _}]= Picture_Line_Campre,
-      ets:update_element(data_base, Picture_Name, [{5, false}]),
-      ets:update_element(data_base, Picture_Name_Cmp, [{5, false}]),
-      gui_server ! {collision, Picture_Name, Mov},
-      gui_server ! {collision, Picture_Name_Cmp, MovCmp}
+      [{Picture_Name, _, _, _}] = Picture_Line,
+      [{Picture_Name_Cmp, _, _, _}]= Picture_Line_Campre,
+      gen_fsm:send_event({global, Picture_Name}, {collision, Mov}),
+      gen_fsm:send_event({global, Picture_Name_Cmp}, {collision, MovCmp}),
+      receive after ?PIC_PROCESS_TIMEOUT -> ok end
   end.
 
 collision_check(Line1, Line2) ->
-  [{_, _, {X1,Y1}, {MovX1, MovY1}, _}] = Line1,
-  [{_, _, {X2,Y2}, {MovX2, MovY2}, Collision_Flag2}] = Line2,
+  [{_, _, {X1,Y1}, {MovX1, MovY1}}] = Line1,
+  [{_, _, {X2,Y2}, {MovX2, MovY2}}] = Line2,
   Ydis = abs(Y1-Y2),
   Xdis = abs(X1-X2),
-  Flag = (Xdis =< ?ImgEdge) and (Ydis =< ?ImgEdge),
-  case ((Collision_Flag2 =:= true) and Flag) of
+  case ((Xdis =< ?ImgEdge) and (Ydis =< ?ImgEdge)) of
     true  ->
       io:format("server colision~n"),
       case (Ydis < Xdis) of
@@ -261,5 +262,3 @@ iterate_update_move([H1 | File_Names], [H2 | File_Names_Dir], Resources_Folder, 
 insert_picture(Picture_Name) ->
   Picture_Atom = list_to_atom(Picture_Name),
   gui_server ! {insert, Picture_Atom, ?PIC_PROCESS_TIMEOUT, ?TTL}.
-
-% ------------------------------------------------- %
