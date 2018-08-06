b3181b6b8dfc13dabf3dea5ac9778d82a77d5a49
bary345 <bary345@gmail.com>
2018-08-06 20:55:10 +0300

    d

--- defines.hrl
+++ defines.hrl
@@ -3,14 +3,15 @@
 -define(RESOURCES, "resources").
 
 -define(BOOM, "BOOM.png").
+-define(KAPAW, "kapaw.png").
+-define(PAW, "paw.png").
 
+-define(STEP, 2).
 -define(ImgEdge, 50).
 -define(RADIUS,  25).
 -define(WIDTH,  1000 ).
 -define(HEIGHT, 800).
 -define(TTL, 3).
-
-% GIT VOOVOVOVOVOVOVOVOVDDDODODOODODO
 
 -define(FILE_SCANNER_DELAY, 1000).
 -define(DRAW_DELAY, 0).
