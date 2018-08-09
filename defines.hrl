57d796b3dab301b6390872750c3d0a7d2301490a
bary345 <bary345@gmail.com>
2018-08-09 19:39:29 +0300

    great

--- defines.hrl
+++ defines.hrl
@@ -1,19 +1,23 @@
--define(BACKGROUND,      "background.jpg").
--define(RECEIVED,  "received").
--define(RESOURCES, "resources").
+-define(BACKGROUND,filename:join(lists:droplast(filename:split(filename:absname(""))))++"/animations/" ++  "background.jpg").
+-define(RECEIVED, filename:join(lists:droplast(filename:split(filename:absname(""))))++ "/received").
+-define(RESOURCES, filename:join(lists:droplast(filename:split(filename:absname(""))))++ "/resources").
+-define(PY_PATH,filename:join(lists:droplast(filename:split(filename:absname(""))))++"/pymodule").
+-define(PY_SERVER_IP,"127.0.0.1").
+-define(Pre_Processed,"unprocessed").
+-define(BOOM,  filename:join(lists:droplast(filename:split(filename:absname(""))))++"/animations/" ++ "BOOM.png").
+-define(KAPAW, filename:join(lists:droplast(filename:split(filename:absname(""))))++"/animations/" ++ "kapaw.png").
+-define(PAW,   filename:join(lists:droplast(filename:split(filename:absname(""))))++"/animations/" ++ "paw.png").
 
--define(BOOM, "BOOM.png").
--define(KAPAW, "kapaw.png").
--define(PAW, "paw.png").
-
--define(STEP, 2).
--define(ImgEdge, 50).
+-define(STEP, 1).
+-define(ImgEdge, 75).
 -define(RADIUS,  25).
--define(WIDTH,  1000 ).
--define(HEIGHT, 800).
+-define(WIDTH,  1500 ).
+-define(HEIGHT, 1200).
 -define(TTL, 3).
 
 -define(FILE_SCANNER_DELAY, 1000).
 -define(DRAW_DELAY, 0).
 -define(GRAPHICS_LOOP_DELAY, 0).
--define(PIC_PROCESS_DELAY, 80).
+-define(PIC_PROCESS_DELAY, 40).
+
+-define(T, 1000).
